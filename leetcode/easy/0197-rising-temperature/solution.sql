1bbd18b2bfba6d6aac6fd43210d80bec3c25b8b3
Bharath-050701 <saisiva06218@gmail.com>
2026-08-29 18:49:24 +0530

    ✅ Rising Temperature | Easy | SQL | Runtime: 573 ms (28.89%) | Memory: 0B | 2026-08-29

--- leetcode/easy/0197-rising-temperature/solution.sql
+++ leetcode/easy/0197-rising-temperature/solution.sql
@@ -1,3 +1,5 @@
-SELECT w1.id
-FROM Weather w1, Weather w2
-WHERE DATEDIFF(w1.recordDate, w2.recordDate) = 1 AND w1.temperature > w2.temperature;
+SELECT t1.id
+FROM Weather t1
+JOIN Weather t2
+  ON DATEDIFF(t1.recordDate, t2.recordDate) = 1
+WHERE t1.temperature > t2.temperature;
